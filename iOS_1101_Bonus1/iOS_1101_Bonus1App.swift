20a743cac740ef9bf9514335d01fe66abca3ed50
Bonnie3206 <molk3206@gmail.com>
2021-11-09 14:35:19 +0800

    Initial Commit

---
+++ iOS_1101_Bonus1/iOS_1101_Bonus1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_1101_Bonus1App.swift
+//  iOS_1101_Bonus1
+//
+//  Created by CK on 2021/11/9.
+//
+
+import SwiftUI
+
+@main
+struct iOS_1101_Bonus1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
