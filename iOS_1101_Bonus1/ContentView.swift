e65b84b3184b94cd3c80998349c8efdcf92ac966
Bonnie3206 <molk3206@gmail.com>
2021-11-09 14:41:03 +0800

    1111109_v1

--- iOS_1101_Bonus1/ContentView.swift
+++ iOS_1101_Bonus1/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var action =  "幹麻呢"
+    @State var option =  ["親親","抱抱","飛高高","親親抱抱飛高高都要!"]
+    //@State var randNum =  1
+    @State var randomElement =  1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("Bouns2")
+                .resizable()
+                .scaledToFit()
+            VStack{
+                Button(action:{
+                    let randNum = 0...3
+                    randomElement = randNum.randomElement()!
+                    action = option[randomElement]
+                }){
+                    Text("今天要幹嘛")
+                        .font(.largeTitle)
+                }.offset(x: 0, y: -330)
+                
+                
+                Text("\(action)")
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .background(Color(red:91/255, green:199/255, blue:199/255))
+                    .cornerRadius(10)
+                    .offset(x: 0, y: 330)
+                
+                
+            }
+        }
     }
 }
 
